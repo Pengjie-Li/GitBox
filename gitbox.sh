0eab9a980201856118025fec7d172b5241631932
Pengjie Li <lipj@connect.hku.hk>
2019-09-04 15:53:55 +0800

    first commit

--- gitbox.sh
+++ gitbox.sh
@@ -1,11 +1,7 @@
-cp $1 /home/lipj/GitBox/
+#cp $1 /home/lipj/GitBox/
 cd ~/GitBox
 git add .
 git commit -m "first commit"
-<<<<<<< HEAD
-git pull origin master
-=======
 git pull origin master 
->>>>>>> 1ca72bdfef37fd3ca253b650bd1a185dfad2cf5a
 git push -u origin master
 cd -
